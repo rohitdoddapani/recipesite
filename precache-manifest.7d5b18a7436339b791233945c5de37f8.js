59a22450d11be93b465477f77d27f8e927fa6b54
Rohit Doddapani <doddapanirohit123@gmail.com>
2019-07-07 22:09:40 +0530

    Updates

---
+++ precache-manifest.7d5b18a7436339b791233945c5de37f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be04c02c9d1c5984baa69787d56bd55",
+    "url": "/recipesite/index.html"
+  },
+  {
+    "revision": "7fe2e7a0aee1eb2200bc",
+    "url": "/recipesite/static/css/main.ed85d32f.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipesite/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "7fe2e7a0aee1eb2200bc",
+    "url": "/recipesite/static/js/main.00e89797.chunk.js"
+  },
+  {
+    "revision": "19942bfc4d49f5094866",
+    "url": "/recipesite/static/js/runtime~main.dea4945b.js"
+  }
+]);
